5be4e045263514bb3af8c76f92fe3175450324b7
MajdAnbtawi <majdanbtawifb@gmail.com>
2023-12-04 00:46:26 +0300

    create sql lite DB

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"majd");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"mohammad");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"mohannad");
